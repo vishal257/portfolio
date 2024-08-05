a7772a407d6455950992de3adc4b77f70139373f
Vishal <vishal2573003@gmail.com>
2024-08-05 19:19:10 +0530

    fixed email issue

--- assets/js/email.js
+++ assets/js/email.js
@@ -16,8 +16,9 @@ const sendEmail = () => {
     <b>Message: </b>${message}`;
 
     Email.send({
-        SecureToken: "ceb7a6fb-1e69-47fa-a1bf-50816ac8135e",
+        // SecureToken: "ceb7a6fb-1e69-47fa-a1bf-50816ac8135e",
         // SecureToken: "83cbc6e3-944e-4a3a-8d13-0a88bd3f113f",
+        SecureToken: "3de8f979-1858-4173-abb6-e110399581be",
         To: "webworks.vishal@gmail.com",
         From: "webworks.vishal@gmail.com",
         Subject: "Enquiry From Portfolio",
